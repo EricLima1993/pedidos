bff2834a5daa3edf1ee4fda232d0f0a91f1152a8
EricLima1993 <merick_lima@hotmail.com>
2026-05-29 10:40:44 -0300

    Criação do fluxo para solicitar pagamento do pedido

--- src/main/java/io/github/ericlima1993/icompras/pedidos/service/PedidoService.java
+++ src/main/java/io/github/ericlima1993/icompras/pedidos/service/PedidoService.java
@@ -1,11 +1,13 @@
 package io.github.ericlima1993.icompras.pedidos.service;
 
+import io.github.ericlima1993.icompras.pedidos.client.ServicoBancarioClient;
 import io.github.ericlima1993.icompras.pedidos.model.Pedido;
 import io.github.ericlima1993.icompras.pedidos.repository.ItemPedidoRepository;
 import io.github.ericlima1993.icompras.pedidos.repository.PedidoRepository;
 import io.github.ericlima1993.icompras.pedidos.validator.PedidoValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,10 +16,23 @@ public class PedidoService {
     private final PedidoRepository pedidoRepository;
     private final ItemPedidoRepository itemPedidoRepository;
     private final PedidoValidator pedidoValidator;
+    private final ServicoBancarioClient servicoBancarioClient;
 
+    @Transactional
     public Pedido criarPedido(Pedido pedido) {
-    pedidoRepository.save(pedido);
-    itemPedidoRepository.saveAll(pedido.getItens());
-    return pedido;
+        pedidoValidator.validar(pedido);
+        realizarPersistencia(pedido);
+        enviarSolicitacaoPagamento(pedido);
+        return pedido;
+    }
+
+    private void enviarSolicitacaoPagamento(Pedido pedido) {
+        var chavePagamento = servicoBancarioClient.solicitarPagamento(pedido);
+        pedido.setChavePagamento(chavePagamento);
+    }
+
+    private void realizarPersistencia(Pedido pedido) {
+        pedidoRepository.save(pedido);
+        itemPedidoRepository.saveAll(pedido.getItens());
     }
 }
