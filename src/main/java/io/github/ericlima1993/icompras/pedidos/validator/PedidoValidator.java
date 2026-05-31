38655dd5293d87580fbcb0d075bbb26da1d32c5f
EricLima1993 <merick_lima@hotmail.com>
2026-05-31 15:04:37 -0300

    Desenvolvimento da validação de Produto

--- src/main/java/io/github/ericlima1993/icompras/pedidos/validator/PedidoValidator.java
+++ src/main/java/io/github/ericlima1993/icompras/pedidos/validator/PedidoValidator.java
@@ -1,12 +1,25 @@
 package io.github.ericlima1993.icompras.pedidos.validator;
 
+import io.github.ericlima1993.icompras.pedidos.client.ProdutosClient;
+import io.github.ericlima1993.icompras.pedidos.client.representation.ProdutoRepresentation;
 import io.github.ericlima1993.icompras.pedidos.model.Pedido;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
+@RequiredArgsConstructor
 public class PedidoValidator {
 
-    public void validar(Pedido pedido) {
+    private final ProdutosClient produtosClient;
 
+    public void validar(Pedido pedido) {
+        List<Long> codigosProdutos = pedido.getItens().stream().map(i -> i.getCodigoProduto()).toList();
+        codigosProdutos.forEach(codigoProduto ->{
+            ResponseEntity<ProdutoRepresentation> response = produtosClient.obterDados(codigoProduto);
+            ProdutoRepresentation produto = response.getBody();
+        });
     }
 }
